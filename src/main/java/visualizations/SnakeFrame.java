537a80a4e2967bd84c3ec3197273dc48f2e20dca
BioMarek <marekkucerabio@gmail.com>
2023-04-21 21:57:03 +0200

    centering fixed

--- src/main/java/visualizations/SnakeFrame.java
+++ src/main/java/visualizations/SnakeFrame.java
@@ -6,9 +6,9 @@ public class SnakeFrame extends JFrame {
     public SnakeFrame(SnakePanel snakePanel) {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(snakePanel);
-        this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
         this.pack();
+        this.setLocationRelativeTo(null);
     }
 }
