1b14d4192b2ac22ae8a0ff2f2b06f1bd884f4136
BioMarek <marekkucerabio@gmail.com>
2023-08-16 21:31:11 +0200

    added more used settings

--- src/main/java/runner/GameConfiguration.java
+++ src/main/java/runner/GameConfiguration.java
@@ -11,7 +11,7 @@ public class GameConfiguration {
     public static List<Configurator> getConfigurations() {
         List<Configurator> configurations = new ArrayList<>();
         configurations.add(multiplayerSettings());
-//        configurations.add(multiplayerSettings2());
+//        configurations.add(multiplayerSettings2Players());
         configurations.add(freeEvolutionSettings());
         return configurations;
     }
@@ -26,6 +26,7 @@ public class GameConfiguration {
             Settings.SNAKE_SIGHT = 7;
             Settings.DEATH_PENALTY = -3;
             Settings.MAX_NUM_OF_FOOD = 150;
+            Settings.STEPS_TO_REDUCTION = 15;
             Settings.LEAVE_CORPSE = false;
             Settings.HAS_WALL = true;
             Settings.SELF_COLLISION = false;
@@ -35,10 +36,70 @@ public class GameConfiguration {
         };
     }
 
-    public static Configurator multiplayerSettings2() {
+    public static Configurator multiplayerSettingsNoWall() {
+        return () -> {
+            Settings.NUM_OF_GENERATIONS = 300;
+            Settings.NUM_OF_PLAYERS = 10;
+            Settings.GRID_COLUMN_PIXELS = 1500;
+            Settings.GRID_ROW_PIXELS = 1080;
+            Settings.PIXELS_PER_SQUARE = 20;
+            Settings.SNAKE_SIGHT = 7;
+            Settings.DEATH_PENALTY = -10;
+            Settings.MAX_NUM_OF_FOOD = 150;
+            Settings.STEPS_TO_REDUCTION = 1000;
+            Settings.LEAVE_CORPSE = false;
+            Settings.HAS_WALL = false;
+            Settings.SELF_COLLISION = false;
+            Settings.SAVE_EVERY_N_GENERATIONS = 100;
+            Settings.SNAKE_SIGHT_TYPE = SnakeSightType.RAYS;
+            Settings.IS_FREE_EVOLUTION = false;
+        };
+    }
+
+    public static Configurator multiplayerSettingsHighDeathPenalty() {
+        return () -> {
+            Settings.NUM_OF_GENERATIONS = 300;
+            Settings.NUM_OF_PLAYERS = 10;
+            Settings.GRID_COLUMN_PIXELS = 1500;
+            Settings.GRID_ROW_PIXELS = 1080;
+            Settings.PIXELS_PER_SQUARE = 20;
+            Settings.SNAKE_SIGHT = 7;
+            Settings.DEATH_PENALTY = -100;
+            Settings.MAX_NUM_OF_FOOD = 150;
+            Settings.STEPS_TO_REDUCTION = 1000;
+            Settings.LEAVE_CORPSE = true;
+            Settings.HAS_WALL = true;
+            Settings.SELF_COLLISION = false;
+            Settings.SAVE_EVERY_N_GENERATIONS = 100;
+            Settings.SNAKE_SIGHT_TYPE = SnakeSightType.RAYS;
+            Settings.IS_FREE_EVOLUTION = false;
+        };
+    }
+
+    public static Configurator multiplayerSettings2Players() {
         return () -> {
             Settings.NUM_OF_GENERATIONS = 300;
             Settings.NUM_OF_PLAYERS = 2;
+            Settings.GRID_COLUMN_PIXELS = 1500;
+            Settings.GRID_ROW_PIXELS = 1080;
+            Settings.PIXELS_PER_SQUARE = 20;
+            Settings.SNAKE_SIGHT = 7;
+            Settings.DEATH_PENALTY = -3;
+            Settings.MAX_NUM_OF_FOOD = 150;
+            Settings.STEPS_TO_REDUCTION = 15;
+            Settings.LEAVE_CORPSE = false;
+            Settings.HAS_WALL = true;
+            Settings.SELF_COLLISION = false;
+            Settings.SAVE_EVERY_N_GENERATIONS = 100;
+            Settings.SNAKE_SIGHT_TYPE = SnakeSightType.RAYS;
+            Settings.IS_FREE_EVOLUTION = false;
+        };
+    }
+
+    public static Configurator singleSnakeSettings() {
+        return () -> {
+            Settings.NUM_OF_GENERATIONS = 300;
+            Settings.NUM_OF_PLAYERS = 10;
             Settings.GRID_COLUMN_PIXELS = 1500;
             Settings.GRID_ROW_PIXELS = 1080;
             Settings.PIXELS_PER_SQUARE = 20;
