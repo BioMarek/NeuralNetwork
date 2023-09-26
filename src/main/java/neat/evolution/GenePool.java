4aec78e04106fd527d351c95c434d737e8a0f0cf
BioMarek <marekkucerabio@gmail.com>
2023-09-26 21:48:59 +0200

    fixed bug when NUM_OF_PLAYERS that doesn't divide TOTAL_NUM_OF_GENOTYPES cause crash

--- src/main/java/neat/evolution/GenePool.java
+++ src/main/java/neat/evolution/GenePool.java
@@ -104,6 +104,15 @@ public class GenePool implements EvolutionEngine {
         }
         result.add(listOfPlayers);
 
+        // handles case where NUM_OF_PLAYERS doesn't divide TOTAL_NUM_OF_GENOTYPES
+        var lastListOfPlayers = result.get(result.size() - 1);
+        var numOfPlayersMissing = Settings.NUM_OF_PLAYERS - lastListOfPlayers.size();
+        if (numOfPlayersMissing > 0) {
+            for (int i = 0; i < numOfPlayersMissing; i++) {
+                lastListOfPlayers.add(listOfPlayers.get(i));
+            }
+        }
+
         return result;
     }
 
