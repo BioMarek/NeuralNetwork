99517c8558aa4daf44c67a455a828e7887cea9d9
BioMarek <marekkucerabio@gmail.com>
2023-10-08 22:02:39 +0200

    test added

--- src/main/java/runner/GameConfiguration.java
+++ src/main/java/runner/GameConfiguration.java
@@ -25,11 +25,11 @@ public class GameConfiguration {
 //        configurations.add(multipleSnakesFillingBoard2());
 //        configurations.add(multipleSnakesFillingBoard3());
 
-        configurations.add(freeEvolutionSettings());
-        configurations.add(freeEvolutionSettings());
-        configurations.add(freeEvolutionSettings());
-        configurations.add(freeEvolutionSettings());
-        configurations.add(freeEvolutionSettings());
+        configurations.add(freeEvolutionSettingsTest());
+        configurations.add(freeEvolutionSettingsTest());
+        configurations.add(freeEvolutionSettingsTest());
+        configurations.add(freeEvolutionSettingsTest());
+        configurations.add(freeEvolutionSettingsTest());
 
         return configurations;
     }
@@ -250,6 +250,29 @@ public class GameConfiguration {
             Settings.SELF_COLLISION = true;
             Settings.SNAKE_SIGHT_TYPE = SnakeSightType.RAYS;
             Settings.IS_FOOD_GUARANTEED = false;
+            Settings.OFFSPRING_THRESHOLD = 0.15d;
+            Settings.MAX_NUM_OF_MOVES = 1000;
+            Settings.IS_FREE_EVOLUTION = true;
+        };
+    }
+
+    public static Configurator freeEvolutionSettingsTest() {
+        return () -> {
+            Settings.SHOW_LEGEND = false;
+            Settings.GRID_COLUMN_PIXELS = 1920;
+            Settings.PIXELS_PER_SQUARE = 20;
+            Settings.NUM_OF_PLAYERS = 25;
+            Settings.CHANCE_MUTATE_WEIGHT = 0.8d;
+            Settings.CHANCE_HARD_MUTATE_WEIGHT = 0.1d;
+            Settings.CHANCE_SWITCH_CONNECTION_ENABLED = 0.2d;
+            Settings.CHANCE_ADD_NODE = 0.03d;
+            Settings.CHANCE_ADD_CONNECTION = 0.03d;
+            Settings.MAX_NUM_OF_FOOD = 200;
+            Settings.LEAVE_CORPSE = false;
+            Settings.HAS_WALL = Util.isRandomChanceTrue(0.5);
+            Settings.SELF_COLLISION = false;
+            Settings.SNAKE_SIGHT_TYPE = SnakeSightType.RAYS;
+            Settings.IS_FOOD_GUARANTEED = Util.isRandomChanceTrue(0.5);
             Settings.OFFSPRING_THRESHOLD = 0.15d;
             Settings.MAX_NUM_OF_MOVES = 1000;
             Settings.IS_FREE_EVOLUTION = true;
